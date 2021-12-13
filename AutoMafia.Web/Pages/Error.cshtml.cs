a4b83260179df603bde0ac19828f80577c51582c
alekzap <zapadinskaia@mail.ru>
2021-12-13 20:10:23 +0300

    added AutoMafia prefix to project names, added stub classes to empty folders

--- AutoMafia.Web/Pages/Error.cshtml.cs
+++ AutoMafia.Web/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Web.Pages;
+namespace AutoMafia.Web.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
