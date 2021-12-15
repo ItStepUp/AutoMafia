d59057f5f1aeb64b55eaf746be6ba516c85e5bc2
Sergey <joker3839@gmail.com>
2021-12-15 18:03:02 +0300

    Add class Player
    Добавлен класс игрок(не закончен, нужна роль)

---
+++ AutoMafia.Domain/Entities/Player.cs
@@ -0,0 +1,15 @@
+﻿namespace AutoMafia.Domain.Entities
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public enum Сondition {Live, Dead }
+
+        public Player(int id, string name, Сondition сondition/*Role*/)
+        {
+            Id = id;
+            Name = name;            
+        }
+    }
+}
