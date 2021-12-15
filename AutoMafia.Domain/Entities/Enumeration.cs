a69fb20aef85b1d2a21986aad59d7ecdc5daea97
TheEvilShot <samofalov.19982805@gmail.com>
2021-12-16 01:04:28 +0300

    Added Role and Enumeration class

---
+++ AutoMafia.Domain/Entities/Enumeration.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+
+namespace AutoMafia.Domain.Entities;
+
+public class Enumeration : IComparable
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public int MovePriority { get; private set; }
+
+    protected Enumeration(Guid id, string name, int priority) => (Id, Name, MovePriority) = (id, name, priority);
+    
+    public static IEnumerable<T> GetAll<T>() where T : Enumeration =>
+        typeof(T).GetFields(BindingFlags.Public |
+                            BindingFlags.Static |
+                            BindingFlags.DeclaredOnly)
+            .Select(f => f.GetValue(null))
+            .Cast<T>();
+    
+    public override bool Equals(object obj)
+    {
+        if (obj is not Enumeration otherValue)
+        {
+            return false;
+        }
+
+        var typeMatches = GetType().Equals(obj.GetType());
+        var valueMatches = Id.Equals(otherValue.Id);
+
+        return typeMatches && valueMatches;
+    }
+
+    public override string ToString() => Name;
+
+    public int CompareTo(object other) => Id.CompareTo(((Enumeration) other).Id);
+}
---
+++ AutoMafia.Domain/Entities/Role.cs
@@ -0,0 +1,16 @@
+﻿namespace AutoMafia.Domain.Entities;
+
+public class Role 
+    : Enumeration
+{
+    public static Role Peaceful = new(new Guid(), nameof(Peaceful), 0);
+    public static Role Maniac = new(new Guid(), nameof(Maniac), 1);
+    public static Role Сourtesan = new(new Guid(), nameof(Сourtesan), 2);
+    public static Role Detective = new(new Guid(), nameof(Detective), 3);
+    public static Role Doctor = new(new Guid(), nameof(Doctor), 4);
+    public static Role Mafia = new(new Guid(), nameof(Mafia), 5);
+    public static Role Don = new(new Guid(), nameof(Don), 6);
+    public Role(Guid id, string name, int priority) : base(id, name, priority)
+    {
+    }
+}
